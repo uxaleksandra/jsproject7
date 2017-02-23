4526c12ca3c1ac3195456e695e6faf4e0d015782
Aleksandra <uxaleksandra@gmail.com>
2017-02-23 08:16:52 +0100

    task

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+var txt ='Papugi koszą trawę, unosząc się nad ziemią 15 centymetrów, na czerwonych dywanach.';
+var animal = 'Zielone słonie'
+var animal = animal.toUpperCase();
+
+var txt = txt.replace('Papugi', animal);
+console.log(txt.substr(0,txt.length/2));
---
+++ readme.md
@@ -0,0 +1,2 @@
+﻿8.8. Łańcuchy znaków
+Zadanie: Papugi i słonie
